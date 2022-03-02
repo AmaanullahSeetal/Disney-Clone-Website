db5b0eaf94d2477885976e2d5a5a19e1ad124bab
Amaan Seetal <macseetal@gmail.com>
2022-03-03 05:59:32 +0800

    Initial Commit

---
+++ components/NavBar.js
@@ -0,0 +1,17 @@
+import Link from 'next/Link'
+import Image from 'next/Image'
+import logo from '../public/disney.png'
+
+const Navbar = ({account}) => {
+    return (
+        <div className="navbar">
+            <Link href="/"><Image src={logo} alt={"Disney Logo"} width={90} height={50}/></Link>        
+            <div className="account-info">
+                <p>Welcome {account.username}</p>
+                <img className="avatar" src={account.avatar.url}/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
